213e7b3a848038e83e06f9c946c24188ca8307db
Peter Mukhortov <muhortov@gmail.com>
2018-03-12 20:52:45 +0200

    update gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -9,56 +9,46 @@ const cssmin = require('gulp-cssmin')
 
 const uglify = require('gulp-uglify')
 const buffer = require('vinyl-buffer')
-const plumber = require('gulp-plumber')
 
-const processTs = debug => {
+const scripts = () => {
 	return browserify({
 		basedir: '.',
-		debug: debug,
-		entries: ['src/ts/main.ts'],
+		debug: false,
+		entries: ['./src/ts/main.ts'],
 		cache: {},
 		packageCache: {}
 	})
-		.plugin(tsify)
-		.bundle()
-		.pipe(source('bundle.js'))
-		.pipe(buffer())
-		.pipe((() => debug ? buffer() : uglify())())
+	.plugin(tsify)
+	.bundle()
+	.on('error', error => {
+		console.log('\x1b[31m' + '[' + error.name + ']: ' + '\x1b[0m', error.message)
+	})
+	.pipe(source('bundle.js'))
+	.pipe(buffer())
+	.pipe(uglify())
+	.on('error', error => {
+		console.log(error)
+	})
+	.pipe(gulp.dest('dist'))
+}
+
+const styles = () => {
+	return gulp.src('src/less/styles.less')
+		.pipe(less())
+		.pipe(prefix('last 8 version', '> 1%'), { cascade: true })
+		.pipe(cssmin())
 		.pipe(gulp.dest('dist'))
 }
 
-gulp.task('copy-html', () => {
+const html = () => {
 	return gulp.src('src/*.html')
 		.pipe(gulp.dest('dist'))
-})
+}
 
-gulp.task('less', () => {
-	return gulp.src('src/less/styles.less')
-		.pipe(plumber())
-		.pipe(less({
-			paths: ['src/less']
-		}))
-		.pipe(prefix('last 8 version', '> 1%'), { cascade: true })
-		.pipe(cssmin().on('error', error => {
-			console.log(error)
-		}))
-		.pipe(gulp.dest('dist'))
-})
+exports.watch = () => {
+	gulp.watch('src/ts/**/*.ts', scripts)
+	gulp.watch('src/less/**/*.less', styles)
+	gulp.watch('src/*.html', html)
+}
 
-gulp.task('watch-ts', () => {
-	gulp.watch('src/**/*.ts', () => processTs(true))
-})
-
-gulp.task('watch-html', () => {
-	gulp.watch('src/**/*.html', ['copy-html'])
-})
-
-gulp.task('watch-less', () => {
-	gulp.watch('src/**/*.less', ['less'])
-})
-
-gulp.task('watch', ['watch-ts', 'watch-html', 'watch-less'])
-
-gulp.task('dev', ['less', 'copy-html'], () => processTs(true))
-
-gulp.task('default', ['less', 'copy-html'], () => processTs(false))
+gulp.task('default', gulp.series(gulp.parallel(styles, scripts, html)))
